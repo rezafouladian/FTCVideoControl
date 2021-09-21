1d0e049da8b0b433e644a3eecdd491da2889adf7
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2021-09-21 06:16:00 -0700

    Generate base ATEM config

--- atem.py
+++ atem.py
@@ -1,7 +1,7 @@
-from configparser import ConfigParser
-import os
+import settings
 
-config = ConfigParser()
+settings.load_config()
 
-if not os.path.exists('config.ini'):
-    config.write(open('config.ini', 'w'))
+if not settings.check_section('atem'):
+    settings.add_section('atem')
+    settings.set_value('atem', 'enable_atem', 'false')
