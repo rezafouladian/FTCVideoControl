740f4002fb1ea854bb226d920ad3a2d2806a5b95
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2021-09-21 03:30:35 -0700

    Create initial testing code

---
+++ main.py
@@ -0,0 +1,54 @@
+import websocket
+
+# Temporary variables
+
+server_name = "localhost"
+
+event_code = "test1"
+
+# API URLs
+
+event_get_all_url = "/api/v1/events/"
+
+
+def event_get_url(code):
+    return "/api/v1/events/" + code + "/"
+
+
+def event_awards_url(code):
+    return "/api/v2/events/" + code + "/awards/"
+
+
+def event_full_details_url(code):
+    return "/api/v2/events/" + code + "/full/"
+
+
+def event_stream_url(code):
+    return "/api/v2/stream/?code=" + code
+
+
+# End API URLs
+
+def on_message(ws, message):
+    return("")
+
+
+def on_error(ws, error):
+    print(error)
+
+
+def on_close(ws):
+    print("Socket Closed")
+
+
+def websocket_test():
+    websocket.enableTrace(True)
+    ws_test = websocket.WebSocketApp(
+        "ws://" + server_name + event_stream_url(event_code),
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close)
+
+
+if __name__ == "__main__":
+    websocket_test()
