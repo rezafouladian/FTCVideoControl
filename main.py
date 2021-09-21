eb5bb9c02cc8211269a7177b57ab468baba8ec2a
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2021-09-21 03:38:20 -0700

    Change to single line functions

--- main.py
+++ main.py
@@ -8,20 +8,16 @@ event_code = "test1"
 event_get_all_url = "/api/v1/events/"
 
 
-def event_get_url(code):
-    return "/api/v1/events/" + code + "/"
+def event_get_url(code): return "/api/v1/events/" + code + "/"
 
 
-def event_awards_url(code):
-    return "/api/v2/events/" + code + "/awards/"
+def event_awards_url(code): return "/api/v2/events/" + code + "/awards/"
 
 
-def event_full_details_url(code):
-    return "/api/v2/events/" + code + "/full/"
+def event_full_details_url(code): return "/api/v2/events/" + code + "/full/"
 
 
-def event_stream_url(code):
-    return "/api/v2/stream/?code=" + code
+def event_stream_url(code): return "/api/v2/stream/?code=" + code
 # End API URLs
 
 
