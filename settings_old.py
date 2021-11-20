561fcebed76879196ee76a4a231410c3a9284091
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2021-11-20 15:47:50 -0800

    Redoing code

--- settings_old.py
+++ settings_old.py
@@ -10,23 +10,29 @@ def write_config():
     config.write(open(config_filename, 'w'))
 
 
-def initialize_config():
+def load_config():
     if not os.path.exists(config_filename):
         write_config()
     config.read(config_filename)
 
 
 def check_section(section):
+    if config.has_section(section):
+        return True
+    else:
+        return False
+
+
+def add_section(section):
     if not config.has_section(section):
         config.add_section(section)
-        write_config()
 
 
-def add_setting(section, setting, value):
-    check_section(section)
-    config.set(section, setting, value)
-    write_config()
+def get_value(section, option):
+    return config.get(section, option)
 
 
-def get_value(section, setting):
-    return config.get(section, setting)
+def set_value(section, option, value):
+    if not config.has_section(section):
+        config.add_section(section)
+    config.set(section, option, value)
